37cabf7652b4d572d06303e4a95bd6498b234aec
hwan6725 <pige0417@naver.com>
2023-10-19 15:42:47 +0900

    스프링부트

---
+++ src/test/java/com/y4k/cyworld/CyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y4k.cyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
